db813aeea4291534901649cf325e87b9323b21c3
mahmoud.ahm.ism <mah.ahm.ism@gmail.com>
2022-12-28 01:04:18 +0200

    Added Worker

--- app/src/main/java/com/example/android/devbyteviewer/work/RefreshDataWork.kt
+++ app/src/main/java/com/example/android/devbyteviewer/work/RefreshDataWork.kt
@@ -16,3 +16,27 @@
  */
 
 package com.example.android.devbyteviewer.work
+
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.example.android.devbyteviewer.database.getDatabase
+import com.example.android.devbyteviewer.repository.VideosRepository
+import retrofit2.HttpException
+
+
+class RefreshDataWorker(context: Context, pram: WorkerParameters) : CoroutineWorker(context, pram) {
+
+    override suspend fun doWork(): Result {
+        val database = getDatabase(applicationContext)
+        val videosRepository = VideosRepository(database)
+
+        return try {
+            videosRepository.refreshVideos()
+            Result.success()
+        } catch (e: HttpException) {
+            Result.retry()
+        }
+    }
+
+}
